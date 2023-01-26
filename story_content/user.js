9a399fe0f79c24071dc758e5bd44b4e582fa11d3
Sittiarifah <99728741+Sittiarifah@users.noreply.github.com>
2023-01-26 13:30:34 +0800

    Add files via upload

---
+++ story_content/user.js
@@ -0,0 +1,18 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6AQuwA36MOT":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+   function play() {
+        var audio = document.getElementById("audio");
+        audio.play();
+      }
+}
+
